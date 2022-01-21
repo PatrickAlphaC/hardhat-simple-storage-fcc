92916055b8d04a47a449f875f6c238736e009701
PatrickAlphac <54278053+PatrickAlphaC@users.noreply.github.com>
2022-01-21 16:41:42 -0500

    fixed gas

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,11 +2,13 @@ require("@nomiclabs/hardhat-waffle")
 require("hardhat-gas-reporter")
 require("./tasks/block-number")
 
+const { task } = require("hardhat/config")
+require("dotenv").config()
+
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners()
-
   for (const account of accounts) {
     console.log(account.address)
   }
@@ -14,17 +16,19 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
-
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || ""
 const KOVAN_RPC_URL =
   process.env.KOVAN_RPC_URL ||
   "https://eth-mainnet.alchemyapi.io/v2/your-api-key"
 const PRIVATE_KEY =
   process.env.PRIVATE_KEY ||
   "0x11ee3108a03081fe260ecdc106554d09d9d1209bcafd46942b10e02943effc4a"
+ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
+
 module.exports = {
   defaultNetwork: "hardhat",
   networks: {
@@ -40,9 +44,9 @@ module.exports = {
   },
   gasReporter: {
     enabled: true,
-    currency: "USD", // why you no work?
+    currency: "USD",
     outputFile: "gas-report.txt",
     noColors: true,
-    coinmarketcap: process.env.COINMARKETCAP_API_KEY,
+    coinmarketcap: COINMARKETCAP_API_KEY,
   },
 }
