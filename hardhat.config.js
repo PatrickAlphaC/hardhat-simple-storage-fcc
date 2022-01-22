e54cddbd0e823ebc7460b3945e69cf7165b47b8b
PatrickAlphac <54278053+PatrickAlphaC@users.noreply.github.com>
2022-01-21 21:34:37 -0500

    updated readme

--- hardhat.config.js
+++ hardhat.config.js
@@ -18,7 +18,7 @@ const KOVAN_RPC_URL =
 const PRIVATE_KEY =
   process.env.PRIVATE_KEY ||
   "0x11ee3108a03081fe260ecdc106554d09d9d1209bcafd46942b10e02943effc4a"
-ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
 
 module.exports = {
   defaultNetwork: "hardhat",
