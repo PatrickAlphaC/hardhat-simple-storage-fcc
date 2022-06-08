5b46ee9e497e8b9ba4aa433710a1f15179abf34f
PatrickAlphac <54278053+PatrickAlphaC@users.noreply.github.com>
2022-06-08 18:18:45 -0400

    updated with a new test for person array and struct

--- test/test-deploy.js
+++ test/test-deploy.js
@@ -27,4 +27,23 @@ describe("SimpleStorage", function () {
     const currentValue = await simpleStorage.retrieve()
     assert.equal(currentValue.toString(), expectedValue)
   })
+
+  // Extra - this is not in the video
+  it("Should work correctly with the people struct and array", async function () {
+    const expectedPersonName = "Patrick"
+    const expectedFavoriteNumber = "16"
+    const transactionResponse = await simpleStorage.addPerson(
+      expectedPersonName,
+      expectedFavoriteNumber
+    )
+    await transactionResponse.wait(1)
+    const { favoriteNumber, name } = await simpleStorage.people(0)
+    // We could also do it like this
+    // const person = await simpleStorage.people(0)
+    // const favNumber = person.favoriteNumber
+    // const pName = person.name
+
+    assert.equal(name, expectedPersonName)
+    assert.equal(favoriteNumber, expectedFavoriteNumber)
+  })
 })
