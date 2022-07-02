a653cf7c6e7e900a7017580521d491dae38801cb
0xSanyam <mahajansanyam.sol@gmail.com>
2022-07-02 11:01:21 -0700

    Updated .env

--- hardhat.config.js
+++ hardhat.config.js
@@ -13,7 +13,7 @@ require("solidity-coverage")
 const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || ""
 const RINKEBY_RPC_URL =
   process.env.RINKEBY_RPC_URL ||
-  "https://eth-mainnet.alchemyapi.io/v2/your-api-key"
+  "https://eth-rinkeby.alchemyapi.io/v2/your-api-key"
 const PRIVATE_KEY =
   process.env.PRIVATE_KEY ||
   "0x11ee3108a03081fe260ecdc106554d09d9d1209bcafd46942b10e02943effc4a"
