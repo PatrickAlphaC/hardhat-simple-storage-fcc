817a607e9be1ca6a9453c2da96c9d45007f4798a
Ali Murtaza Memon <apple@alimurtazamemon.local>
2022-09-21 19:39:35 +0500

    update chain id from 4 rinkeby to 5 goerli

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -9,7 +9,7 @@ async function main() {
   await simpleStorage.deployed()
   console.log(`Deployed contract to: ${simpleStorage.address}`)
   // what happens when we deploy to our hardhat network?
-  if (network.config.chainId === 4 && process.env.ETHERSCAN_API_KEY) {
+  if (network.config.chainId === 5 && process.env.ETHERSCAN_API_KEY) {
     console.log("Waiting for block confirmations...")
     await simpleStorage.deployTransaction.wait(6)
     await verify(simpleStorage.address, [])
