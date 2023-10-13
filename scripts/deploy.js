ecb08b685220d64053e4d787f207b02d19e91525
Sarthak <sarthakrawatbiz@gmail.com>
2023-10-13 06:59:55 +0530

    Using new functions like waitForDepolyment() and .target that are compatible with hardhat-toolbox@3.0

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,28 +3,27 @@ const { ethers, run, network } = require("hardhat")
 
 // async main
 async function main() {
-    const SimpleStorageFactory = await ethers.getContractFactory(
-        "SimpleStorage"
-    )
-    console.log("Deploying contract...")
-    const simpleStorage = await SimpleStorageFactory.deploy()
-    await simpleStorage.waitForDeployment()
-    console.log(`Deployed contract to: ${simpleStorage.target}`)
-    // what happens when we deploy to our hardhat network?
-    if (network.config.chainId === 11155111 && process.env.ETHERSCAN_API_KEY) {
-        console.log("Waiting for block confirmations...")
-        await simpleStorage.deployTransaction.wait(6)
-        await verify(simpleStorage.address, [])
-    }
+  const SimpleStorageFactory = await ethers.getContractFactory("SimpleStorage")
+  console.log("Deploying contract...")
+  const simpleStorage = await SimpleStorageFactory.deploy()
+  await simpleStorage.waitForDeployment()
+  console.log(`Deployed contract to: ${simpleStorage.target}`)
+  
+  // what happens when we deploy to our hardhat network?
+  if (network.config.chainId === 11155111 && process.env.ETHERSCAN_API_KEY) {
+    console.log("Waiting for block confirmations...")
+    await simpleStorage.deployTransaction.wait(6)
+    await verify(simpleStorage.address, [])
+  }
 
-    const currentValue = await simpleStorage.retrieve()
-    console.log(`Current Value is: ${currentValue}`)
+  const currentValue = await simpleStorage.retrieve()
+  console.log(`Current Value is: ${currentValue}`)
 
-    // Update the current value
-    const transactionResponse = await simpleStorage.store(7)
-    await transactionResponse.wait(1)
-    const updatedValue = await simpleStorage.retrieve()
-    console.log(`Updated Value is: ${updatedValue}`)
+  // Update the current value
+  const transactionResponse = await simpleStorage.store(7)
+  await transactionResponse.wait(1)
+  const updatedValue = await simpleStorage.retrieve()
+  console.log(`Updated Value is: ${updatedValue}`)
 }
 
 // async function verify(contractAddress, args) {
